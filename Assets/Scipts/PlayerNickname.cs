79080f68256a8e01e58e323f4eb196148d2948d8
5LRaf <115564919+5LRaf@users.noreply.github.com>
2026-06-18 07:03:15 -0300

    fix nickname colors

--- Assets/Scipts/PlayerNickname.cs
+++ Assets/Scipts/PlayerNickname.cs
@@ -1,15 +1,55 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using TMPro;
+using Photon.Realtime;
+using Hashtable = ExitGames.Client.Photon.Hashtable;
 
-public class PlayerNickname : MonoBehaviourPun
+public class PlayerNickname : MonoBehaviourPunCallbacks
 {
     [SerializeField] private TMP_Text nicknameText;
+
+    [Header("Team Colors")]
+    [SerializeField] private Color team0Color = Color.blue;
+    [SerializeField] private Color team1Color = Color.red;
+
+    private int team = -1;
 
     private void Start()
     {
         nicknameText.text = photonView.Owner.NickName;
+        TrySetTeamColor();
+    }
+
+    private void TrySetTeamColor()
+    {
+        if (photonView.Owner.CustomProperties != null &&
+            photonView.Owner.CustomProperties.TryGetValue("team", out object t))
+        {
+            team = (int)t;
+            ApplyColor();
+        }
+        else
+        {
+            Invoke(nameof(TrySetTeamColor), 0.5f);
+        }
+    }
+
+    private void ApplyColor()
+    {
+        nicknameText.color = (team == 0) ? team0Color : team1Color;
+
+        Debug.Log($"[NICKNAME] {photonView.Owner.NickName} Team {team}");
+    }
+
+    public override void OnPlayerPropertiesUpdate(Photon.Realtime.Player targetPlayer, Hashtable changedProps)
+    {
+        if (targetPlayer != photonView.Owner)
+            return;
+
+        if (changedProps.ContainsKey("team"))
+        {
+            team = (int)changedProps["team"];
+            ApplyColor();
+        }
     }
 }
