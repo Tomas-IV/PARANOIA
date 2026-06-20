d9d8058f0e5a03f442397296c1cf6844b5d83bd6
5LRaf <115564919+5LRaf@users.noreply.github.com>
2026-06-19 21:15:39 -0300

    api

--- Assets/Scipts/ChuckNorris.cs
+++ Assets/Scipts/ChuckNorris.cs
@@ -2,17 +2,48 @@ using UnityEngine.Networking;
 using Newtonsoft.Json;
 using UnityEngine;
 using System.Collections;
-public class ChuckNorrisJokeConsumer : MonoBehaviour
+public class ChuckNorris : MonoBehaviour
 {
     private string url = "https://api.chucknorris.io/jokes/random";
-    public IEnumerator GetJoke()
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            StartCoroutine(GetJoke((joke) =>
+            {
+                Debug.Log(joke);
+            }));
+        }
+    }
+
+    public IEnumerator GetJoke(System.Action<string> onResult)
     {
         using (UnityWebRequest req = UnityWebRequest.Get(url))
         {
             yield return req.SendWebRequest();
+
+            if (req.result != UnityWebRequest.Result.Success)
+            {
+                Debug.LogError("Error API: " + req.error);
+                onResult?.Invoke("Error al obtener chiste");
+            }
+            else
+            {
+                string json = req.downloadHandler.text;
+
+                ChuckNorrisData data =
+                    JsonConvert.DeserializeObject<ChuckNorrisData>(json);
+
+                onResult?.Invoke(data.value);
+            }
         }
+
     }
 }
+
+[System.Serializable]
 public class ChuckNorrisData
 {
+    public string value;
 }
